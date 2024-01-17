8e9d1614955eb5b9c69bfdb00cc183c62f604774
beeb@neb.one <mercedesmwit20@gmail.com>
2024-01-17 16:10:13 +0700

    flusher

--- flusher/flusher/handler.py
+++ flusher/flusher/handler.py
@@ -117,12 +117,16 @@ class Handler(object):
         ).scalar()
 
     def handle_new_block(self, msg):
-        self.conn.execute(insert(blocks).values(msg).on_conflict_do_update(constraint='blocks_pkey', set_=msg))
+        self.conn.execute(
+            insert(blocks)
+            .values(msg)
+            .on_conflict_do_update(constraint="blocks_pkey", set_=msg)
+        )
 
     def handle_new_transaction(self, msg):
         msg["memo"] = msg["memo"].replace("\x00", "\uFFFD")
         if msg["err_msg"] is not None:
-            msg["err_msg"] = msg["err_msg"].replace("\x00","\uFFFD")
+            msg["err_msg"] = msg["err_msg"].replace("\x00", "\uFFFD")
         msg["sender"] = self.get_account_id(msg["sender"])
         self.conn.execute(
             insert(transactions)
@@ -162,7 +166,11 @@ class Handler(object):
             select([accounts.c.id]).where(accounts.c.address == msg["address"])
         ).scalar()
         if id is None:
-            self.conn.execute(insert(accounts).values(msg).on_conflict_do_update(constraint='accounts_pkey', set_=msg))
+            self.conn.execute(
+                insert(accounts)
+                .values(msg)
+                .on_conflict_do_update(constraint="accounts_pkey", set_=msg)
+            )
         else:
             msg["id"] = id
             self.conn.execute(accounts.update(accounts.c.id == msg["id"]).values(**msg))
@@ -175,11 +183,7 @@ class Handler(object):
         )
 
     def handle_update_code_instantiate_config(self, msg):
-        self.conn.execute(
-            codes.update()
-            .where(codes.c.id == msg["id"])
-            .values(**msg)
-        )
+        self.conn.execute(codes.update().where(codes.c.id == msg["id"]).values(**msg))
 
     def handle_update_contract(self, id):
         self.conn.execute(
@@ -338,17 +342,21 @@ class Handler(object):
         self.conn.execute(pools.insert(), msg)
 
     def handle_update_set_superfluid_asset(self, msg):
-        self.conn.execute(pools.update().where(pools.c.id == msg["id"]).values({"is_superfluid": True}))
-
-    def handle_update_remove_superfluid_asset(self, msg):
-        self.conn.execute(pools.update().where(pools.c.id == msg["id"]).values({"is_superfluid": False}))
-
-    def handle_update_pool(self, msg):
         self.conn.execute(
             pools.update()
             .where(pools.c.id == msg["id"])
-            .values(**msg)
+            .values({"is_superfluid": True})
         )
+
+    def handle_update_remove_superfluid_asset(self, msg):
+        self.conn.execute(
+            pools.update()
+            .where(pools.c.id == msg["id"])
+            .values({"is_superfluid": False})
+        )
+
+    def handle_update_pool(self, msg):
+        self.conn.execute(pools.update().where(pools.c.id == msg["id"]).values(**msg))
 
     def handle_new_pool_transaction(self, msg):
         msg["transaction_id"] = self.get_transaction_id(msg["tx_hash"])
@@ -363,14 +371,21 @@ class Handler(object):
     def handle_set_taker_fee(self, msg):
         fee = self.conn.execute(
             select([taker_fee.c.taker_fee]).where(
-                (taker_fee.c.denom0 == msg["denom0"]) & (taker_fee.c.denom1 == msg["denom1"]))
+                (taker_fee.c.denom0 == msg["denom0"])
+                & (taker_fee.c.denom1 == msg["denom1"])
+            )
         ).scalar()
         if fee is None:
             self.conn.execute(taker_fee.insert(), msg)
         else:
-            self.conn.execute(taker_fee.update()
-                              .where((taker_fee.c.denom0 == msg["denom0"]) & (taker_fee.c.denom1 == msg["denom1"]))
-                              .values(taker_fee=msg["taker_fee"]))
+            self.conn.execute(
+                taker_fee.update()
+                .where(
+                    (taker_fee.c.denom0 == msg["denom0"])
+                    & (taker_fee.c.denom1 == msg["denom1"])
+                )
+                .values(taker_fee=msg["taker_fee"])
+            )
 
     def handle_set_validator(self, msg):
         msg["account_id"] = self.get_account_id(msg["delegator_address"])
@@ -388,4 +403,57 @@ class Handler(object):
             validators.update()
             .where(validators.c.operator_address == msg["operator_address"])
             .values(**msg)
+        )
+
+    def handle_genesis_proposal(self, msg):
+        msg["proposer_id"] = self.get_account_id(msg["proposer"])
+        del msg["proposer"]
+        self.conn.execute(
+            proposals.insert().on_conflict_do_nothing(constraint="proposals_pkey"),
+            msg,
+        )
+
+    def handle_genesis_osmosis_pool(self, msg):
+        msg["creator"] = self.get_account_id(msg["creator"])
+        if "create_tx" in msg and msg["create_tx"] is not None:
+            msg["create_tx_id"] = self.get_transaction_id(msg["create_tx"])
+            del msg["create_tx"]
+        else:
+            msg["create_tx_id"] = None
+        self.conn.execute(
+            pools.insert().on_conflict_do_nothing(constraint="pools_pkey"), msg
+        )
+
+    def handle_genesis_validator(self, msg):
+        msg["account_id"] = self.get_account_id(msg["delegator_address"])
+        del msg["delegator_address"]
+        if self.get_validator_id(msg["operator_address"]) is None:
+            self.conn.execute(validators.insert(), msg)
+
+    def handle_genesis_code(self, msg):
+        if "tx_hash" in msg and msg["tx_hash"] is not None:
+            msg["transaction_id"] = self.get_transaction_id(msg["tx_hash"])
+            del msg["tx_hash"]
+        else:
+            msg["transaction_id"] = None
+
+        msg["uploader"] = self.get_account_id(msg["uploader"])
+        self.conn.execute(
+            codes.insert().on_conflict_do_nothing(constraint="codes_pkey"), msg
+        )
+
+    def handle_genesis_contract(self, msg):
+        if "tx_hash" in msg and msg["tx_hash"] is not None:
+            msg["init_tx_id"] = self.get_transaction_id(msg["tx_hash"])
+            del msg["tx_hash"]
+        else:
+            msg["init_tx_id"] = None
+
+        msg["init_by"] = self.get_account_id(msg["init_by"])
+        if msg["admin"] != "":
+            msg["admin"] = self.get_account_id(msg["admin"])
+        else:
+            del msg["admin"]
+        self.conn.execute(
+            contracts.insert().on_conflict_do_nothing(constraint="contracts_pkey"), msg
         )
